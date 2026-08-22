f70a578531324c04e378e692e216e33d17964e8e
Okidi-richard <richardokidi1@gmail.com>
2026-08-22 21:39:49 +0100

    Add render_template_string import to app.py

--- app/app.py
+++ app/app.py
@@ -18,7 +18,7 @@ if str(ROOT_DIR) not in sys.path:
     sys.path.insert(0, str(ROOT_DIR))
 
 from flask import (
-    Flask, render_template, request, redirect, url_for,
+    Flask, render_template, render_template_string,request, redirect, url_for,
     flash, send_file, send_from_directory, jsonify, session
 )
 import pandas as pd
