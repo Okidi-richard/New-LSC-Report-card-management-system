058d355ce3b9d59258123c719764919414c3ad58
Okidi-richard <richardokidi1@gmail.com>
2026-08-25 21:31:11 +0100

    Fix indentation for database operations in app.py

--- app/app.py
+++ app/app.py
@@ -755,11 +755,12 @@ if photo and photo.filename:
                 active=True
             )
 
-            db.session.add(student)
-            db.session.commit()
+        db.session.add(student)
+        db.session.commit()
 
         return redirect(url_for("manage_students"))
-            students = Student.query.filter_by(
+
+    students = Student.query.filter_by(
         school_id=user.school_id,
         active=True
     ).order_by(Student.full_name.asc()).all()
